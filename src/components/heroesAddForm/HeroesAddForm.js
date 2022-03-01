bbb164c2d155531746249f93d83218c94d313d97
Матвей Калинин <matvey.kalinin2001@gmail.com>
2022-03-01 12:10:49 +0300

    create hero

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,4 +1,11 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { useHttp } from "../../hooks/http.hook";
+import {
+  heroesFetching,
+  heroesFetched,
+  heroesFetchingError,
+} from "../../actions";
 
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
@@ -11,48 +18,81 @@
 // данных из фильтров
 
 const HeroesAddForm = () => {
-    return (
-        <form className="border p-4 shadow-lg rounded">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
-                    required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
-            </div>
+  const { heroes } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { request } = useHttp();
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    formState: { isSubmitSuccessful },
+  } = useForm();
 
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label fs-4">Описание</label>
-                <textarea
-                    required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
-                    placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
-            </div>
+  const onSubmit = (body) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes", "POST", JSON.stringify(body))
+      .then((data) => dispatch(heroesFetched([...heroes, data])))
+      .catch(() => dispatch(heroesFetchingError()));
+    if (isSubmitSuccessful) {
+      resetField("name");
+      resetField("description");
+      resetField("element");
+    }
+  };
 
-            <div className="mb-3">
-                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
-                    required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
-                    <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
-                </select>
-            </div>
+  return (
+    <form
+      className="border p-4 shadow-lg rounded"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label fs-4">
+          Имя нового героя
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          placeholder="Как меня зовут?"
+          {...register("name", { required: true })}
+        />
+      </div>
 
-            <button type="submit" className="btn btn-primary">Создать</button>
-        </form>
-    )
-}
+      <div className="mb-3">
+        <label htmlFor="text" className="form-label fs-4">
+          Описание
+        </label>
+        <textarea
+          className="form-control"
+          id="text"
+          placeholder="Что я умею?"
+          style={{ height: "130px" }}
+          {...register("description", { required: true })}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="element" className="form-label">
+          Выбрать элемент героя
+        </label>
+        <select
+          className="form-select"
+          id="element"
+          {...register("element", { required: true })}
+        >
+          <option value="">Я владею элементом...</option>
+          <option value="fire">Огонь</option>
+          <option value="water">Вода</option>
+          <option value="wind">Ветер</option>
+          <option value="earth">Земля</option>
+        </select>
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Создать
+      </button>
+    </form>
+  );
+};
 
 export default HeroesAddForm;
--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,35 +1,43 @@
 import { useCallback } from "react";
 
 export const useHttp = () => {
-    // const [process, setProcess] = useState('waiting');
+  // const [process, setProcess] = useState('waiting');
 
-    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+  const request = useCallback(
+    async (
+      url,
+      method = "GET",
+      body = null,
+      headers = { "Content-Type": "application/json" }
+    ) => {
+      // setProcess('loading');
 
-        // setProcess('loading');
+      try {
+        const response = await fetch(url, { method, body, headers });
 
-        try {
-            const response = await fetch(url, {method, body, headers});
-
-            if (!response.ok) {
-                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
-            }
-
-            const data = await response.json();
-
-            return data;
-        } catch(e) {
-            // setProcess('error');
-            throw e;
+        if (!response.ok) {
+          throw new Error(`Could not fetch ${url}, status: ${response.status}`);
         }
-    }, []);
 
-    // const clearError = useCallback(() => {
-        // setProcess('loading');
-    // }, []);
+        const data = await response.json();
 
-    return {request, 
-            // clearError, 
-            // process, 
-            // setProcess
-        }
-}
+        return data;
+      } catch (e) {
+        // setProcess('error');
+        throw e;
+      }
+    },
+    []
+  );
+
+  // const clearError = useCallback(() => {
+  // setProcess('loading');
+  // }, []);
+
+  return {
+    request,
+    // clearError,
+    // process,
+    // setProcess
+  };
+};
